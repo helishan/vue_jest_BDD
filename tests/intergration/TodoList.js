8137c82d8071ee56ab62d7d42b205ea197234917
helishan <2436129132@qq.com>
2022-10-13 16:07:16 +0800

    first commit

---
+++ tests/intergration/TodoList.js
@@ -0,0 +1,18 @@
+import { mount } from '@vue/test-utils'
+import { findTestWrapper } from '../../src/utils/testUtils'
+import TodoList from '../../src/containers/TodoList/TodoList.vue'
+
+it(`
+  1. 用户会在 header 输入框输入内容
+  2. 用户会点击回车按钮
+  3. 列表项应该增加用户输入内容的列表项
+`, () => {
+  const wrapper = mount(TodoList)
+  const inputElem = findTestWrapper(wrapper, 'header-input').at(0)
+  const content = 'Dell lee'
+  inputElem.setValue(content)
+  inputElem.trigger('change')
+  inputElem.trigger('keyup.enter')
+  const listItems = findTestWrapper(wrapper, 'list-item')
+  expect(listItems.length).toBe(1)
+})
